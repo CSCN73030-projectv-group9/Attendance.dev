355791eea062c0de818abed828b7305a1461e672
ehharvey <eharvey3704@conestogac.on.ca>
2022-11-01 06:00:35 -0400

    Set dev's email

--- utility-scripts/postAttachCommand.sh
+++ utility-scripts/postAttachCommand.sh
@@ -1,5 +1,13 @@
 #! /bin/bash
 
+declare -A dev_emails=( 
+  ["ehharvey"]="eharvey3704@conestogac.on.ca" 
+  ["Amanuel-Negussie"]="Anegussie9077@conestogac.on.ca" 
+  ["colefoster"]="Cfoster5841@conestogac.on.ca" 
+  ["Justin200104"]="Jlangevin@conestogac.on.ca"
+)
+
+
 pip install -r requirements.txt
 
 gh auth status
@@ -9,3 +17,9 @@ then
   echo "gh is not authenticated!"
   gh auth login
 fi
+
+username=$(gh api user | jq -r '.login')
+email=${dev_emails[$username]}
+
+git config user.name $username
+git config user.email $email
